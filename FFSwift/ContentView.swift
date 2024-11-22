e8580a0a635d8acecd57cc9bebd2d60e7c8831c4
Jake Sulpice <jake@jakesulpice.com>
2024-11-21 18:52:16 -0800

    Initial Commit

---
+++ FFSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FFSwift
+//
+//  Created by Jake Sulpice on 11/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FFSwift/FFSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  FFSwiftApp.swift
+//  FFSwift
+//
+//  Created by Jake Sulpice on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct FFSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
